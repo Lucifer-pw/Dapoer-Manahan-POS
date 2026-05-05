7d8e1a42098840c9d9b1ae1c3676584714b00332
Lucifer <ranggadhuk@gmail.com>
2026-05-05 11:57:12 +0700

    My Watermark & Fitur LastLogin

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -283,6 +283,29 @@ class _LoginScreenState extends State<LoginScreen>
                         ],
                       ),
                     ),
+                    const SizedBox(height: 60),
+                    Text(
+                      'Powered by LUCIFAX',
+                      style: AppTextStyles.caption.copyWith(
+                        color: AppColors.textHint.withOpacity(0.5),
+                        letterSpacing: 2,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 10,
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                    Center(
+                      child: Text(
+                        'Powered by LUCIFAX',
+                        style: AppTextStyles.caption.copyWith(
+                          color: AppColors.textHint.withOpacity(0.5),
+                          letterSpacing: 2,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 10,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
                   ],
                 ),
               ),
